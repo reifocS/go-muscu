eb3fae7de90c455eada19f140f8ab38de64f321a
reifocs <alainvincentesc@gmail.com>
2022-04-03 12:51:44 +0000

    month calendar

--- app/routes/calendar.tsx
+++ app/routes/calendar.tsx
@@ -1,94 +1,95 @@
-import { useStore } from "~/store";
+import dayjs, { Dayjs } from "dayjs";
+import { useState } from "react";
+
 
 function App() {
-  const start = useStore((state) => state.startDate);
-  const end = useStore((state) => state.endDate);
-  const nextMonth = useStore((state) => state.nextMonth);
-  const prevMonth = useStore((state) => state.prevMonth);
+  /*
+  const start = useStore(selectorStart)
+  const nextMonth = useStore(selectorPrev)
+  const prevMonth = useStore(selectorPrev)
   const dayInMonth = start.daysInMonth();
+  const weekInMonth = Math.floor(dayInMonth / 7);
+*/
+  const [startDate, setStartDate] = useState(() => dayjs().startOf("month"))
 
   return (
-    <div className="w-full">
-      <div className="flex flex-1"></div>
-      <h1>Calendar</h1>
-      <p>{`${start}-${end}`}</p>
-      <button onClick={prevMonth}>prevMonth</button>
-      <button onClick={nextMonth}>nextMonth</button>
-      <table
-        className="w-full"
-        cellSpacing="21" cellPadding="21">
-        <thead>
-          <tr>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>sat</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>1</td>
-            <td>2</td>
-          </tr>
-          <tr></tr>
-          <tr>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>11</td>
-            <td>12</td>
-            <td>13</td>
-            <td>14</td>
-            <td>15</td>
-            <td>16</td>
-          </tr>
-          <tr>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-            <td>20</td>
-            <td>21</td>
-            <td>22</td>
-            <td>23</td>
-          </tr>
-          <tr>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-            <td>27</td>
-            <td>28</td>
-            <td>29</td>
-            <td>30</td>
-          </tr>
-          <tr>
-            <td>31</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+      <TableMonth startDate={startDate} />
+      <br />
+      <h3>{startDate.format("MMMM")}</h3>
+      <button onClick={() => setStartDate(prev => prev.subtract(1, "month"))}>{"<"}</button>
+      <button onClick={() => setStartDate(prev => prev.add(1, "month"))}>{">"}</button>
+    </>
   );
+}
+
+const daysInWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const TableHead = () => {
+  return (
+    <thead className="bg-gray-50">
+      <tr>
+        {daysInWeek.map(d => <th key={d} className="px-6 py-2 text-md text-gray-500">{d}</th>
+        )}
+      </tr>
+    </thead>
+  )
+}
+
+const weekArray: Array<number> = Array(7).fill(0);
+
+const Cell = ({ day, isPast }: { day?: Dayjs, isPast: boolean }) => {
+
+  return (
+    <td
+      className={`px-6 py-4 ${isPast && "text-gray-500"}`}
+    >{day?.format("DD") ?? ""}</td>
+  )
+}
+
+const Week = ({ weekNumber, daysInMonth, startDate }: { weekNumber: number, daysInMonth: Array<Dayjs>, startDate: Dayjs }) => {
+  return <tr className="whitespace-nowrap">
+    {
+      weekArray.map((_, index) => {
+        const day = weekNumber * 7 + (index + 1);
+        const isPast = daysInMonth[day]?.isBefore(startDate);
+        return (
+          <Cell day={daysInMonth[day]} key={index} isPast={isPast} />
+        )
+      })}
+  </tr>
+}
+
+const getAllDaysInMonth = (startDate: dayjs.Dayjs, daysInMonth: number): Array<Dayjs> => {
+  let days = []
+  const firstDayIndex = startDate.day();
+  for (let i = firstDayIndex + 1; i > 0; --i) {
+    days.push(startDate.subtract(i, "day"));
+  }
+  for (let i = 0; i < daysInMonth; ++i) {
+    days.push(startDate.add(i, "day"));
+  }
+  return days;
+}
+
+const weeks = Array(6).fill(0);
+
+const TableMonth = ({ startDate }: { startDate: dayjs.Dayjs }) => {
+  const dayInMonth = startDate.daysInMonth();
+  const allDaysInMonth = getAllDaysInMonth(startDate, dayInMonth);
+  return (<table
+    className="w-full table-auto divide-y divide-gray-300 ">
+
+    <TableHead />
+    <tbody className="bg-white divide-y divide-gray-300 text-center">
+      {weeks.map((_, i) => {
+        return (
+          <Week key={i} weekNumber={i} startDate={startDate} daysInMonth={allDaysInMonth} />
+        )
+      })}
+    </tbody>
+  </table>
+  )
 }
 
 export default App;
