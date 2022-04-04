4ef10db124a01cf4ea07145147c11031d818b281
reifocs <alainvincentesc@gmail.com>
2022-04-04 10:15:40 +0000

    add link to login when no user

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,14 @@
+import { Link } from "remix";
+import { useOptionalUser } from "~/utils";
+
 export default function Index() {
+  const user = useOptionalUser();
   return (
     <main>
       <h1>Workouts</h1>
+      {!user && <Link to="login">
+        Log in
+      </Link>}
     </main>
   );
 }
