a4e1f2d2067941531494802621f0360ec02c2835
mlhoutel <mlhoutel@gmail.com>
2022-04-07 20:04:43 +0200

    allow to have multiple series of same exercise

--- app/routes/daily.tsx
+++ app/routes/daily.tsx
@@ -1,61 +1,8 @@
-/* import dayjs from "dayjs";
-import {
-  ActionFunction,
-  Form,
-  json,
-  LoaderFunction,
-  Outlet,
-  redirect,
-  useLoaderData,
-} from "remix";
-import {
-  createWorkout,
-  getDailyWorkout,
-  Workout,
-} from "~/models/workout.server";
-import { requireUserId } from "~/session.server";
-
-type LoaderData = {
-  dailyWorkout: Workout | null;
-};
-
-export const action: ActionFunction = async ({ request }) => {
-  const userId = await requireUserId(request);
-  const today = dayjs().startOf("day").toDate();
-
-  const workout = await createWorkout({ date: today, userId });
-  return redirect(`/daily/${workout.id}`);
-};
-
-export const loader: LoaderFunction = async ({ request, params }) => {
-  const userId = await requireUserId(request);
-  const todayMidnight = dayjs().startOf("day");
-  const tomorrowMidnight = dayjs(todayMidnight.add(1));
-  let dailyWorkout = await getDailyWorkout({
-    userId,
-    dateStart: todayMidnight.toDate(),
-    dateEnd: tomorrowMidnight.toDate(),
-  });
-  if (!dailyWorkout) {
-    dailyWorkout = await createWorkout({
-      date: todayMidnight.toDate(),
-      userId,
-    });
-  }
-  //redirect and prevent infinite redirection
-  if (dailyWorkout && !params.workoutId) {
-    return redirect(`daily/${dailyWorkout.id}`);
-  }
-  return json<LoaderData>({ dailyWorkout });
-};*/
-
 import dayjs from "dayjs";
 import type { ActionFunction, LoaderFunction } from "remix";
 import { Form, json, redirect, useCatch, useLoaderData } from "remix";
-import invariant from "tiny-invariant";
 import {
   deleteWorkout,
-  getWorkout,
   Workout,
   createWorkout,
   getDailyWorkout,
@@ -278,12 +225,7 @@ export default function WorkoutDetailsPage() {
           </h3>
         </div>
       </Form>
-      <Carrousel
-        workoutId={data.workout.id}
-        elementList={data.exerciseList.filter(
-          (ex) => !data.workout.set.find((set) => set.exerciseId === ex.id)
-        )}
-      />
+      <Carrousel workoutId={data.workout.id} elementList={data.exerciseList} />
       {data.workout.set.map((s) => (
         <div className="my-2" key={s.id}>
           <div className="flex items-center justify-center">
