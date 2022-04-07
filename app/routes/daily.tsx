67132897580fc0bcb55978225d8d11f967def09c
mlhoutel <mlhoutel@gmail.com>
2022-04-07 19:31:32 +0200

    Unification de la page daily

--- app/routes/daily.tsx
+++ app/routes/daily.tsx
@@ -1,12 +1,23 @@
 import dayjs from "dayjs";
-import {ActionFunction, Form, json, LoaderFunction, Outlet, redirect, useLoaderData} from "remix";
-import {createWorkout, getDailyWorkout, Workout} from "~/models/workout.server";
-import {requireUserId} from "~/session.server";
-import {useOptionalUser} from "~/utils";
+import {
+  ActionFunction,
+  Form,
+  json,
+  LoaderFunction,
+  Outlet,
+  redirect,
+  useLoaderData,
+} from "remix";
+import {
+  createWorkout,
+  getDailyWorkout,
+  Workout,
+} from "~/models/workout.server";
+import { requireUserId } from "~/session.server";
 
 type LoaderData = {
-  dailyWorkout: Workout | null
-}
+  dailyWorkout: Workout | null;
+};
 
 export const action: ActionFunction = async ({ request }) => {
   const userId = await requireUserId(request);
@@ -19,31 +30,41 @@ export const action: ActionFunction = async ({ request }) => {
 export const loader: LoaderFunction = async ({ request, params }) => {
   const userId = await requireUserId(request);
   const todayMidnight = dayjs().startOf("day");
-  const tomorrowMidnight = dayjs(todayMidnight.add(1))
-  const dailyWorkout = await getDailyWorkout({ userId, dateStart: todayMidnight.toDate(), dateEnd: tomorrowMidnight.toDate() });
+  const tomorrowMidnight = dayjs(todayMidnight.add(1));
+  let dailyWorkout = await getDailyWorkout({
+    userId,
+    dateStart: todayMidnight.toDate(),
+    dateEnd: tomorrowMidnight.toDate(),
+  });
+  if (!dailyWorkout) {
+    dailyWorkout = await createWorkout({
+      date: todayMidnight.toDate(),
+      userId,
+    });
+  }
   //redirect and prevent infinite redirection
   if (dailyWorkout && !params.workoutId) {
-    return redirect(`daily/${dailyWorkout.id}`)
+    return redirect(`daily/${dailyWorkout.id}`);
   }
   return json<LoaderData>({ dailyWorkout });
 };
 
 export default function Index() {
-  const user = useOptionalUser();
   const data = useLoaderData<LoaderData>();
 
   return (
     <main className="p-4">
       <div className={`${data.dailyWorkout ? "hidden" : ""}`}>
         <Form method="post">
-
           <button
-            className="h-10 px-3 m-1 text-green-100 transition-colors duration-150 bg-green-700 rounded-lg focus:shadow-outline hover:bg-green-800"
-            type="submit">Tiens, voila pour toi!</button>
+            className="focus:shadow-outline m-1 h-10 rounded-lg bg-green-700 px-3 text-green-100 transition-colors duration-150 hover:bg-green-800"
+            type="submit"
+          >
+            Tiens, voila pour toi!
+          </button>
         </Form>
       </div>
       <Outlet />
-
     </main>
   );
 }
