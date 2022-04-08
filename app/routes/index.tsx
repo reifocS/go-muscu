1db4cba2236726b4288166099c7029636dad88bc
mlhoutel <mlhoutel@gmail.com>
2022-04-08 13:49:52 +0200

    Fix pipeline

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,8 @@
-import {Link} from "remix";
-import {useOptionalUser} from "~/utils";
+import { Link, redirect } from "remix";
+import { useOptionalUser } from "~/utils";
 
 export default function Index() {
   const user = useOptionalUser();
 
-  return (
-    <main>
-      <h1>GO MUSCU!!!</h1>
-      {!user && <Link to="login">Log in</Link>}
-
-
-    </main>
-  );
+  return <main>{!user && <Link to="login">Log in</Link>}</main>;
 }
