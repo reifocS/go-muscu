b2c0fa991947875aa26699325baddeee1d417329
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2022-04-11 15:01:39 +0200

    pwa !!

--- app/routes/resources/manifest[.]json.ts
+++ app/routes/resources/manifest[.]json.ts
@@ -1,72 +1,54 @@
-import { json } from "remix";
-import type { LoaderFunction } from "remix";
+import type {LoaderFunction} from "remix";
+import {json} from "remix";
 
 export let loader: LoaderFunction = () => {
-  return json(
-    {
-      short_name: "PWA",
-      name: "Remix PWA",
-      start_url: "/",
-      display: "standalone",
-      background_color: "#d3d7dd",
-      theme_color: "#c34138",
-      shortcuts: [
+    return json(
         {
-          name: "Homepage",
-          url: "/",
-          icons: [
-            {
-              src: "/icons/android-icon-96x96.png",
-              sizes: "96x96",
-              type: "image/png",
-              purpose: "any monochrome",
+            short_name: "Go muscu",
+            name: "Go muscu",
+            start_url: "/",
+            display: "standalone",
+            background_color: "#d3d7dd",
+            theme_color: "#c34138",
+            shortcuts: [
+                {
+                    name: "Homepage",
+                    url: "/",
+                    icons: [
+                        {
+                            "src": "/icons/icon-192x192.png",
+                            "sizes": "192x192",
+                            "type": "image/png"
+                        },
+                    ],
+                },
+            ], icons: [
+                {
+                    "src": "/icons/icon-192x192.png",
+                    "sizes": "192x192",
+                    "type": "image/png"
+                },
+                {
+                    "src": "/icons/icon-256x256.png",
+                    "sizes": "256x256",
+                    "type": "image/png"
+                },
+                {
+                    "src": "/icons/icon-384x384.png",
+                    "sizes": "384x384",
+                    "type": "image/png"
+                },
+                {
+                    "src": "/icons/icon-512x512.png",
+                    "sizes": "512x512",
+                    "type": "image/png"
+                }
+            ]
+        },
+        {
+            headers: {
+                "Cache-Control": "public, max-age=600",
             },
-          ],
         },
-      ],
-      icons: [
-        {
-          src: "/icons/android-icon-36x36.png",
-          sizes: "36x36",
-          type: "image/png",
-          density: "0.75",
-        },
-        {
-          src: "/icons/android-icon-48x48.png",
-          sizes: "48x48",
-          type: "image/png",
-          density: "1.0",
-        },
-        {
-          src: "/icons/android-icon-72x72.png",
-          sizes: "72x72",
-          type: "image/png",
-          density: "1.5",
-        },
-        {
-          src: "/icons/android-icon-96x96.png",
-          sizes: "96x96",
-          type: "image/png",
-          density: "2.0",
-        },
-        {
-          src: "/icons/android-icon-144x144.png",
-          sizes: "144x144",
-          type: "image/png",
-          density: "3.0",
-        },
-        {
-          src: "/icons/android-icon-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-          density: "4.0",
-        },
-      ],
-    },
-    {
-      headers: {
-        "Cache-Control": "public, max-age=600",
-      },
-    },
-  );
+    );
 };
