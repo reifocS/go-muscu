80381168f6ee59d1cb6dde3a85acd803a160f9cb
mlhoutel <mlhoutel@gmail.com>
2022-04-13 18:13:07 +0200

    Fix notes removal

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -24,22 +24,6 @@ async function seed() {
     },
   });
 
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
   const workout = await prisma.workout.create({
     data: {
       userId: user.id,
