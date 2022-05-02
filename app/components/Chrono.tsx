a1af2c1ff8d8c6071425b2ca03e643e9f5bf0b83
mlhoutel <mlhoutel@gmail.com>
2022-05-02 14:51:25 +0200

    Global chrono

---
+++ app/components/Chrono.tsx
@@ -0,0 +1,138 @@
+import { useState } from "react";
+import Popover from "~/components/Popover";
+import Chronometre from "~/components/Chronometre";
+import { useCountUpdater } from "~/contexts/useTimer";
+
+type Props = {};
+
+export default function Chrono({}: Props) {
+  const [showDialog, setShowDialog] = useState(false);
+  const setTime = useCountUpdater();
+  const [min, setMin] = useState(0);
+  const [sec, setSeconds] = useState(0);
+  //const [lastTime, setLastTime] = useState<number | null>(null);
+  const open = () => setShowDialog(true);
+  const close = () => setShowDialog(false);
+
+  const setTimeAndStore = (time: number) => {
+    setTime({ timer: time, finished: false });
+    //setLastTime(time);
+  };
+
+  return (
+    <div>
+      <button
+        style={{
+          position: "absolute",
+          bottom: 10,
+          right: 10,
+        }}
+        className="flex h-[60px] w-[60px] items-center justify-center rounded-full bg-blue-500 p-4 font-bold font-bold text-white transition-colors duration-150"
+        onClick={open}
+      >
+        <Chronometre />
+      </button>
+
+      <Popover isOpen={showDialog}>
+        <div className="w-full flex-col items-center justify-center text-right">
+          <button
+            className="h-8 w-8 self-end rounded-full pt-0.5 text-center text-xl font-bold text-white"
+            onClick={close}
+          >
+            <span aria-hidden>x</span>
+          </button>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setTimeAndStore(min * 60 + sec);
+              close();
+            }}
+          >
+            <div className="flex w-full items-center justify-center text-center text-5xl">
+              <div className="w-25 mx-1 rounded-lg p-2">
+                <label
+                  htmlFor="minutes"
+                  className="block text-sm font-medium text-gray-200"
+                >
+                  Minutes
+                </label>
+                <input
+                  name="minutes"
+                  value={String(min).padStart(2, "0")}
+                  onChange={(e) => setMin(+e.target.value)}
+                  type="number"
+                  min={0}
+                  className="w-full rounded bg-gray-900 text-center font-mono leading-none"
+                  max={60}
+                  placeholder="min"
+                />
+              </div>
+              <div className="mx-1 text-2xl">:</div>
+              <div className="w-25 mx-1 rounded-lg p-2">
+                <label
+                  htmlFor="seconds"
+                  className="block text-sm font-medium text-gray-200"
+                >
+                  Seconds
+                </label>
+                <input
+                  name="seconds"
+                  value={String(sec).padStart(2, "0")}
+                  onChange={(e) => setSeconds(+e.target.value)}
+                  type="number"
+                  className="w-full bg-gray-900 text-center font-mono leading-none"
+                  min={0}
+                  max={60}
+                  placeholder="seconds"
+                />
+              </div>
+            </div>
+
+            <div className="text-center">
+              <button
+                type="button"
+                className="m-2 rounded-full bg-gray-700 py-2 px-2 font-bold text-white"
+                onClick={() => {
+                  setTimeAndStore(90);
+                  close();
+                }}
+              >
+                1:30
+              </button>
+              <button
+                type="button"
+                className="m-2 rounded-full bg-gray-700  py-2 px-2 font-bold text-white hover:bg-blue-700"
+                onClick={() => {
+                  setTimeAndStore(2 * 60 + 30);
+                  close();
+                }}
+              >
+                2:30
+              </button>
+              <button
+                type="button"
+                className="m-2 rounded-full bg-gray-700 py-2 px-2 font-bold text-white hover:bg-gray-800"
+                onClick={() => {
+                  setTimeAndStore(3 * 60 + 30);
+                  close();
+                }}
+              >
+                3:30
+              </button>
+            </div>
+
+            <button
+              className="
+              mt-2 flex 
+              w-full items-center justify-center rounded bg-gray-700 py-2 px-4
+              font-bold text-white text-white"
+              type="submit"
+            >
+              START
+            </button>
+          </form>
+        </div>
+      </Popover>
+    </div>
+  );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,117 +1,121 @@
-import type {LinksFunction, LoaderFunction, MetaFunction} from "remix";
+import type { LinksFunction, LoaderFunction, MetaFunction } from "remix";
 import {
-    json,
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-    useLocation,
-    useMatches,
-    useNavigate,
+  json,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLocation,
+  useMatches,
+  useNavigate,
 } from "remix";
 import React from "react";
-import {useOptionalUser} from "~/utils";
-import reactToastifyCss from 'react-toastify/dist/ReactToastify.css';
+import { useOptionalUser } from "~/utils";
+import reactToastifyCss from "react-toastify/dist/ReactToastify.css";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import tailwindGlobalCss from "./styles/global.css";
 import reactResizableCss from "react-resizable/css/styles.css";
 
-import {getUser} from "./session.server";
-import {Header} from "./components/Header";
-import {IoIosArrowBack} from "react-icons/io";
-import {ToastContainer} from "react-toastify";
-import {CountProvider} from "~/contexts/useTimer";
+import { getUser } from "./session.server";
+import { Header } from "./components/Header";
+import { IoIosArrowBack } from "react-icons/io";
+import { ToastContainer } from "react-toastify";
+import { CountProvider } from "~/contexts/useTimer";
+import Chrono from "./components/Chrono";
 
 export const links: LinksFunction = () => {
-    return [
-        {rel: "stylesheet", href: tailwindStylesheetUrl},
-        {rel: "stylesheet", href: tailwindGlobalCss},
-        {rel: "stylesheet", href: reactResizableCss},
-        {rel: "stylesheet", href: reactToastifyCss}
-    ];
+  return [
+    { rel: "stylesheet", href: tailwindStylesheetUrl },
+    { rel: "stylesheet", href: tailwindGlobalCss },
+    { rel: "stylesheet", href: reactResizableCss },
+    { rel: "stylesheet", href: reactToastifyCss },
+  ];
 };
 export const meta: MetaFunction = () => ({
-    charset: "utf-8",
-    title: "Go muscu",
-    viewport: "width=device-width,initial-scale=1",
+  charset: "utf-8",
+  title: "Go muscu",
+  viewport: "width=device-width,initial-scale=1",
 });
 type LoaderData = { user: Awaited<ReturnType<typeof getUser>> };
-export const loader: LoaderFunction = async ({request}) => {
-    return json<LoaderData>({user: await getUser(request)});
+export const loader: LoaderFunction = async ({ request }) => {
+  return json<LoaderData>({ user: await getUser(request) });
 };
 export default function App() {
-    const user = useOptionalUser();
-    let location = useLocation();
-    let matches = useMatches();
-    let navigate = useNavigate();
+  const user = useOptionalUser();
+  let location = useLocation();
+  let matches = useMatches();
+  let navigate = useNavigate();
 
-    let isMount = true;
-    React.useEffect(() => {
-        let mounted = isMount;
-        isMount = false;
-        if ("serviceWorker" in navigator) {
-            if (navigator.serviceWorker.controller) {
-                navigator.serviceWorker.controller?.postMessage({
-                    type: "REMIX_NAVIGATION",
-                    isMount: mounted,
-                    location,
-                    matches,
-                    manifest: window.__remixManifest,
-                });
-            } else {
-                let listener = async () => {
-                    await navigator.serviceWorker.ready;
-                    navigator.serviceWorker.controller?.postMessage({
-                        type: "REMIX_NAVIGATION",
-                        isMount: mounted,
-                        location,
-                        matches,
-                        manifest: window.__remixManifest,
-                    });
-                };
-                navigator.serviceWorker.addEventListener("controllerchange", listener);
-                return () => {
-                    navigator.serviceWorker.removeEventListener(
-                        "controllerchange",
-                        listener
-                    );
-                };
-            }
-        }
-    }, [location]);
+  let isMount = true;
+  React.useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: "REMIX_NAVIGATION",
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: "REMIX_NAVIGATION",
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener("controllerchange", listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener(
+            "controllerchange",
+            listener
+          );
+        };
+      }
+    }
+  }, [location]);
 
-    return (
-        <html lang="en">
-        <head>
-            <link rel="manifest" href="/resources/manifest.json"/>
-            <Meta/>
-            <Links/>
-        </head>
-        <body className="relative h-screen overflow-hidden bg-gray-800 font-mono text-white">
+  return (
+    <html lang="en">
+      <head>
+        <link rel="manifest" href="/resources/manifest.json" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="relative h-screen overflow-hidden bg-gray-800 font-mono text-white">
         <CountProvider>
-        <Header user={user}/>
-        <button onClick={() => navigate(-1)} className={"p-2 md:hidden"}>
-            <IoIosArrowBack/>
-        </button>
-            <Outlet/>
+          <Header user={user} />
+          <button onClick={() => navigate(-1)} className={"p-2 md:hidden"}>
+            <IoIosArrowBack />
+          </button>
+
+          <Outlet />
+          <Chrono />
         </CountProvider>
-        <ScrollRestoration/>
-        <Scripts/>
-        <LiveReload/>
+
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
         <ToastContainer
-            position="top-right"
-            autoClose={1500}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
+          position="top-right"
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
         />
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
