457a15a58ffc4d6f5790d1853253f6a7c90c2eba
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2022-05-13 16:57:40 +0200

    slider for chrono

--- app/components/Chronometre.tsx
+++ app/components/Chronometre.tsx
@@ -1,8 +1,9 @@
 import {useState} from "react";
-import Popover from "~/components/Popover";
 import {useCountState, useCountUpdater} from "~/contexts/useTimer";
 import {RiTimerLine} from "react-icons/ri";
 import dayjs from "dayjs";
+import TimeSlider from "./TimeSlider";
+import {Dialog} from "@reach/dialog";
 
 type Props = {};
 
@@ -19,12 +20,12 @@ export function prettyPrint(time: number) {
 export default function Chrono({}: Props) {
     const [showDialog, setShowDialog] = useState(false);
     const setTime = useCountUpdater();
-    const [min, setMin] = useState<string | null>("01:30");
+    const [min, setMin] = useState(0);
+    const [sec, setSec] = useState(0);
 
     const count = useCountState();
     const isTimer = count != null && !count.finished;
 
-    //const [lastTime, setLastTime] = useState<number | null>(null);
     const open = () => setShowDialog(true);
     const close = () => setShowDialog(false);
 
@@ -32,6 +33,12 @@ export default function Chrono({}: Props) {
         setTime({timer: time, finished: false, endTime: dayjs().add(time, "seconds")});
         //setLastTime(time);
     };
+
+    function onSubmit() {
+        setTimeAndStore(min * 60 + sec);
+        close();
+    }
+
 
     return (
         <div>
@@ -43,7 +50,16 @@ export default function Chrono({}: Props) {
                 <p className="text-sm">{isTimer && prettyPrint(count?.timer)}</p>
             </button>
 
-            <Popover isOpen={showDialog}>
+            <Dialog aria-label={"chrono"} style={{
+                backgroundColor: "#374151",
+                maxWidth: '450px',
+                opacity: 1,
+                borderRadius: '14px',
+                paddingBottom: '3.5em',
+                boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
+                margin: '10vh auto',
+                width: "100%",
+            }} isOpen={showDialog}>
                 <div className="w-full flex-col items-center justify-center text-right">
                     <button
                         className="h-8 w-8 self-end rounded-full pt-0.5 text-center text-xl font-bold text-white"
@@ -51,85 +67,54 @@ export default function Chrono({}: Props) {
                     >
                         <span aria-hidden>x</span>
                     </button>
-                    <form
-                        onSubmit={(e) => {
-                            e.preventDefault();
+                    <div className="w-full">
+                        <TimeSlider setMin={setMin} setSec={setSec}/>
+                    </div>
 
-                            const minsec = min?.split(":");
-
-                            if (!minsec) {
-                                setTimeAndStore(0);
-                            } else {
-                                setTimeAndStore(Number(minsec[0]) * 60 + Number(minsec[1]));
-                            }
-                            close();
-                        }}
-                    >
-                        <div className="w-full">
-                            <div className="flex flex-col items-center justify-center text-center ">
-                                <label
-                                    htmlFor="appt-time"
-                                    className="mb-[70px] text-sm font-medium text-gray-200"
-                                >
-                                    min:sec
-                                </label>
-                                <input
-                                    className="rounded bg-gray-900 text-center font-mono text-5xl"
-                                    id="appt-time"
-                                    type="time"
-                                    name="appt-time"
-                                    style={{maxWidth: 300}}
-                                    value={String(min)}
-                                    onChange={(e) => setMin(e.target.value)}
-                                />
-                            </div>
-                        </div>
-
-                        <div className="text-center">
-                            <button
-                                type="button"
-                                className="m-2 rounded-full bg-gray-700 py-2 px-2 font-bold text-white"
-                                onClick={() => {
-                                    setTimeAndStore(90);
-                                    close();
-                                }}
-                            >
-                                1:30
-                            </button>
-                            <button
-                                type="button"
-                                className="m-2 rounded-full bg-gray-700  py-2 px-2 font-bold text-white hover:bg-blue-700"
-                                onClick={() => {
-                                    setTimeAndStore(2 * 60 + 30);
-                                    close();
-                                }}
-                            >
-                                2:30
-                            </button>
-                            <button
-                                type="button"
-                                className="m-2 rounded-full bg-gray-700 py-2 px-2 font-bold text-white hover:bg-gray-800"
-                                onClick={() => {
-                                    setTimeAndStore(3 * 60 + 30);
-                                    close();
-                                }}
-                            >
-                                3:30
-                            </button>
-                        </div>
-
+                    <div className="text-center">
                         <button
-                            className="
-              mt-2 flex 
-              w-full items-center justify-center rounded bg-gray-700 py-3 px-4
-              font-bold text-white text-white"
-                            type="submit"
+                            type="button"
+                            className="m-2 rounded-full bg-blue-700 hover:bg-blue-800 py-2 px-2 font-bold text-white"
+                            onClick={() => {
+                                setTimeAndStore(90);
+                                close();
+                            }}
                         >
-                            START
+                            1:30
                         </button>
-                    </form>
+                        <button
+                            type="button"
+                            className="m-2 rounded-full bg-blue-700  py-2 px-2 font-bold text-white hover:bg-blue-800"
+                            onClick={() => {
+                                setTimeAndStore(2 * 60 + 30);
+                                close();
+                            }}
+                        >
+                            2:30
+                        </button>
+                        <button
+                            type="button"
+                            className="m-2 rounded-full bg-blue-700 py-2 px-2 font-bold text-white hover:bg-blue-800"
+                            onClick={() => {
+                                setTimeAndStore(3 * 60 + 30);
+                                close();
+                            }}
+                        >
+                            3:30
+                        </button>
+                    </div>
+
+                    <button
+                        className="
+              mt-2 flex hover:bg-blue-800
+              w-full items-center justify-center rounded bg-blue-700 py-3 px-4
+              font-bold text-white text-white"
+                        onClick={onSubmit}
+                    >
+                        START
+                    </button>
                 </div>
-            </Popover>
+            </Dialog>
         </div>
     );
 }
