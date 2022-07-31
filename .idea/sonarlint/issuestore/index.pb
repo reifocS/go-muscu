ef85210a5db4f8028eade06628649dca3c605fb1
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2022-07-31 11:36:21 +0200

    generate report

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,11 +1,3 @@
 
-D
-app/routes/index.tsx,d\6\d649aa1c35a4a277e0721965caa77d26ac4b6d99
-F
-app/routes/workout.tsx,d\9\d9abc5e3412cdc7a21d97eb8aa7437b9a3681513
-R
-"app/routes/calendar/$workoutId.tsx,d\7\d724464c87246f4e99468422769ca95769e450fb
-K
-app/routes/calendar/new.tsx,5\8\58020b034ab6068f325a297a89776c59ecc67737
 <
 .env.example,d\4\d4dae00d11854b35292c2b8a30515a9a0aa2d871
---
+++ app/routes/report.tsx
@@ -0,0 +1,79 @@
+import * as React from "react";
+import dayjs from "dayjs";
+import {Form, json, LoaderFunction, useLoaderData} from "remix";
+import {requireUserId} from "~/session.server";
+import {getAllInRange} from "~/models/workout.server";
+
+export const loader: LoaderFunction = async ({request}) => {
+    const userId = await requireUserId(request);
+    const url = new URL(request.url);
+    let startMonthFromParams = url.searchParams.get("month");
+    let startMonth;
+    if (!startMonthFromParams) {
+        startMonth = dayjs().startOf("month");
+    } else {
+        startMonth = dayjs(startMonthFromParams).startOf("month");
+    }
+    const endOfMonth = startMonth.endOf("month");
+    const allInMonth = await getAllInRange({
+        dateStart: startMonth.toDate(),
+        dateEnd: endOfMonth.toDate(),
+        userId
+    })
+    const exerciseWithAmount: Record<string, number> = {};
+    for (const w of allInMonth) {
+        for (const s of w.set) {
+            if (exerciseWithAmount[s.exercise.title] !== undefined) {
+                exerciseWithAmount[s.exercise.title] = exerciseWithAmount[s.exercise.title] + s.series.length;
+            } else {
+                exerciseWithAmount[s.exercise.title] = 0;
+            }
+        }
+    }
+    return json({
+        allInMonth,
+        month: startMonth.format("YYYY-MM"),
+        nbOfWorkouts: allInMonth.length,
+        exerciseWithAmount
+    });
+};
+
+export default function Rapport() {
+    const data = useLoaderData();
+    console.log(data)
+    return (
+        <>
+            <Form
+                method="get"
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: 8,
+                }}
+            >
+                <p>
+                    you trained {data.nbOfWorkouts} times in {dayjs(data.month).format("MMMM")}
+                </p>
+                <label htmlFor="start">Start month:</label>
+                <input type="month" id="month" name="month"
+                       className="text-black"
+                       required
+                       defaultValue={data.month}/>
+                <button
+                    type="submit"
+                    className="my-2 inline-flex items-center justify-center bg-blue-500 p-2 font-bold text-blue-100 transition-colors duration-150 hover:bg-blue-600"
+                >
+                    Generate report
+                </button>
+
+                    <ul>
+                        {Object.entries(data.exerciseWithAmount).map(([k, v]) => <li key={k}>
+                            {k}: {v} series
+                        </li>)}
+                    </ul>
+            </Form>
+        </>
+    )
+}
