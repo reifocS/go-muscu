d910de6bb81dcf9bcdcd74108272dc1e559d0edd
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2022-08-01 18:56:20 +0200

    report

--- app/models/workout.server.ts
+++ app/models/workout.server.ts
@@ -1,113 +1,143 @@
-import type { User, Workout } from "@prisma/client";
+import type {User, Workout} from "@prisma/client";
 
-import { prisma } from "~/db.server";
+import {prisma} from "~/db.server";
 
-export type { Workout } from "@prisma/client";
+export type {Workout} from "@prisma/client";
 
 export function getDailyWorkout({
-  dateEnd,
-  dateStart,
-  userId,
-}: {
-  userId: User["id"];
-  dateStart: Date;
-  dateEnd: Date;
+                                    dateEnd,
+                                    dateStart,
+                                    userId,
+                                }: {
+    userId: User["id"];
+    dateStart: Date;
+    dateEnd: Date;
 }) {
-  return prisma.workout.findFirst({
-    where: {
-      userId,
-      date: {
-        gte: dateStart,
-        lte: dateEnd,
-      },
-    },
-    include: {
-      set: {
-        include: {
-          series: true,
-          exercise: {
-            select: { title: true },
-          },
+    return prisma.workout.findFirst({
+        where: {
+            userId,
+            date: {
+                gte: dateStart,
+                lte: dateEnd,
+            },
         },
-      },
-    },
-  });
+        include: {
+            set: {
+                include: {
+                    series: true,
+                    exercise: {
+                        select: {title: true},
+                    },
+                },
+            },
+        },
+    });
 }
 
 export function getWorkout({
-  id,
-  userId,
-}: Pick<Workout, "id"> & {
-  userId: User["id"];
+                               id,
+                               userId,
+                           }: Pick<Workout, "id"> & {
+    userId: User["id"];
 }) {
-  return prisma.workout.findFirst({
-    where: { id, userId },
-    include: {
-      set: {
+    return prisma.workout.findFirst({
+        where: {id, userId},
         include: {
-          series: true,
-          exercise: {
-            select: { title: true },
-          },
+            set: {
+                include: {
+                    series: true,
+                    exercise: {
+                        select: {title: true},
+                    },
+                },
+            },
         },
-      },
-    },
-  });
+    });
 }
 
-export function getWorkoutList({ userId }: { userId: User["id"] }) {
-  return prisma.workout.findMany({
-    where: { userId },
-    orderBy: { date: "desc" },
-    include: {
-      set: {
-        select: {
-          exercise: {
-            select: {
-              title: true,
+export function getWorkoutList({userId}: { userId: User["id"] }) {
+    return prisma.workout.findMany({
+        where: {userId},
+        orderBy: {date: "desc"},
+        include: {
+            set: {
+                select: {
+                    exercise: {
+                        select: {
+                            title: true,
+                        },
+                    },
+                },
             },
-          },
         },
-      },
-    },
-  });
+    });
 }
 
 export function createWorkout({
-  date,
-  userId,
-}: Pick<Workout, "date"> & {
-  userId: User["id"];
+                                  date,
+                                  userId,
+                              }: Pick<Workout, "date"> & {
+    userId: User["id"];
 }) {
-  return prisma.workout.create({
-    data: {
-      date,
-      user: {
-        connect: {
-          id: userId,
+    return prisma.workout.create({
+        data: {
+            date,
+            user: {
+                connect: {
+                    id: userId,
+                },
+            },
         },
-      },
-    },
-    select: {
-      id: true,
-      date: true,
-      set: {
-        include: {
-          series: true,
-          exercise: {
-            select: { title: true },
-          },
+        select: {
+            id: true,
+            date: true,
+            set: {
+                include: {
+                    series: true,
+                    exercise: {
+                        select: {title: true},
+                    },
+                },
+            },
         },
-      },
-    },
-  });
+    });
 }
 
 export function deleteWorkout({
-  id,
-  userId,
-}: Pick<Workout, "id"> & { userId: User["id"] }) {
-  return prisma.workout.deleteMany({
-    where: { id, userId },
-  });
+                                  id,
+                                  userId,
+                              }: Pick<Workout, "id"> & { userId: User["id"] }) {
+    return prisma.workout.deleteMany({
+        where: {id, userId},
+    });
+}
+
+export function getAllInRange({
+                                  dateEnd,
+                                  dateStart,
+                                  userId,
+                              }: {
+    userId: User["id"];
+    dateStart: Date;
+    dateEnd: Date;
+}) {
+    return prisma.workout.findMany({
+        where: {
+            userId,
+            date: {
+                gte: dateStart,
+                lte: dateEnd,
+            },
+        },
+        include: {
+            set: {
+                include: {
+                    series: true,
+                    exercise: {
+                        select: {title: true},
+                    },
+                },
+            },
+        },
+    });
 }
