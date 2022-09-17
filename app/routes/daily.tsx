9c42a6e4459b6cde85c9b08f0ad7b6ed5d4d6caf
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2022-09-17 09:34:32 +0200

    reduce input search size

--- app/routes/daily.tsx
+++ app/routes/daily.tsx
@@ -319,7 +319,7 @@ export default function WorkoutDetailsPage() {
                     </div>
                     <input type="search" id="default-search" defaultValue={data.exerciseQuery ?? ""}
                            name={"exerciseQuery"}
-                           className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           className="block h-[30px] p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                            placeholder="Filter exercise.." required/>
                 </div>
             </Form>
